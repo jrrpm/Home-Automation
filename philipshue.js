16a75a42de1e2d5d6110467649c2de8cadeec169
jrrpm <rodrigopintomendez@hotmail.com>
2016-05-24 23:44:51 -0400

    Create philipshue.js

---
+++ philipshue.js
@@ -0,0 +1,56 @@
+var request = require('request');
+
+var hue = new function() {
+
+    var tUser = "xMjcTqqAixNzXCaY51G6IUhVyB1lCeam-JppPTkL";
+    var aUrl = "http://192.168.1.11/api/";
+    var eLog = false;
+    var lightsID = [1,2,3];
+
+    function sendRequest(command, params)
+    {
+        request({
+            url: aUrl+tUser+command,
+            method: 'PUT',
+            json: params
+        }, function(err, response) {
+            if (eLog) {
+                if (err) console.log(err);
+                else console.log(response.body);
+            }
+      });  
+    }
+
+    this.allLightsOff = function() {
+        if (eLog) console.log("philips off");
+        var json = {
+            "on": false
+        };
+        for (var i in lightsID) sendRequest("/lights/"+lightsID[i]+"/state", json);
+    }
+
+    this.allLightsOn = function() {
+        if (eLog) console.log("philips on");
+        var json = {
+            "on": true,
+            "bri": 254,
+            "hue": 11483,
+            "sat": 254
+        };
+        for (var i in lightsID) sendRequest("/lights/"+lightsID[i]+"/state", json);
+    }
+    
+    this.allLightsBright = function(value) {
+        if (eLog) console.log("philips bright:" + value);
+        var json = {
+            "on": true,
+            "bri": value,
+            "hue": 11483,
+            "sat": 254
+        };
+        for (var i in lightsID) sendRequest("/lights/"+lightsID[i]+"/state", json);
+    }
+
+};
+
+module.exports = hue;
