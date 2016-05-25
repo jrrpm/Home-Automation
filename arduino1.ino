f0faa59689908058b2e53eff42b2ea9c2a0fd71c
jrrpm <rodrigopintomendez@hotmail.com>
2016-05-25 00:44:22 -0400

    Update arduino1.ino

--- arduino1.ino
+++ arduino1.ino
@@ -1,16 +1,24 @@
 const int rango = 15;
-int luzDimmer = A0;
-int lightBit = 5;
+const int luzDimmer = A0;
+const int soundSensor = A1;
+const int lightBit = 5;
+
 float valorActualLuzDimmer = 0;
 float valorLuzDimmer = 0;
+bool soundSensorOn = false;
+bool lockSensor = false;
+
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(lightBit, OUTPUT ) ;
   pinMode(luzDimmer, INPUT ) ;
+  pinMode(soundSensor, INPUT ) ;
 }
 
 void loop() {
+  
+  if (sensorChanged()) cambiarLuzSensor();
   if (cambioLuzDimmer()) cambiarLuzDimmer();
 }
 
@@ -25,9 +33,40 @@ void cambiarLuzDimmer()
   delay(200);
   if (cambioLuzDimmer()) {
     valorActualLuzDimmer = valorLuzDimmer;
-    float escribir = (valorActualLuzDimmer/1023)*45 + 210;
-    analogWrite(lightBit, escribir);
-    delay(200);
-    analogWrite(lightBit, 0);
+    writeBit(lightBit, calculateDimmerBitOutput());
   }
+}
+
+bool sensorChanged()
+{
+  if (!lockSensor)
+  {
+    int valorSensor = analogRead(soundSensor);  
+    if (valorSensor>500) {
+      lockSensor = true;
+      return true;
+    }      
+  }
+  return false;
+}
+
+void cambiarLuzSensor()
+{
+  if (soundSensorOn) writeBit(lightBit, 200);
+  else writeBit(lightBit, calculateDimmerBitOutput());
+  soundSensorOn = !soundSensorOn; 
+  delay(3000);
+  lockSensor = false;
+}
+
+int calculateDimmerBitOutput()
+{
+  return (valorActualLuzDimmer/1023)*45 + 210;
+}
+
+void writeBit(int cloudbit, int value)
+{
+  analogWrite(cloudbit, value); 
+  delay(200);
+  analogWrite(cloudbit, 0);  
 }
